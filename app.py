29ecd5c33e56908d52a18d76bce431eabc555233
dreais <valentin.lattay@gmail.com>
2020-03-13 18:47:35 +0100

    structuring API

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 from flask import Flask
 from flask import render_template
+import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/account")
+def account():
+    id = db.get_user_id("test@test")
+    return "Hello / account" + str(id)
+
+@app.route("/reader")
+def reader():
+    return "Hello / reader"
+
+@app.route("/reader/<id>")
+def read_id(id):
+    return "Hello / read id=" + str(id)
+
+## API
+@app.route("/acc/create")
+@app.route("/acc/login")
+def create_acc():
+    return "Hello / create acc"
